7e8ae5588125cde301120f8590a3fcef6da488de
randa yahya <randaalrayan96@gmial.com>
2020-07-30 00:26:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 // Add your functions here
-[1, 2, 3, -9].map(function(a) {
-   return -1 * a
- });
-// function mapToNegativize(src) {
-//   let r = []
-//   for (let i = 0; i < src.length; i++ ) {
-//     r.push(-1 * src[i]) // Unique work
-//   }
-//   return r
-// }
-
-
-// dune = ["paul", "gurney", "vladimir", "jessica", "chani"]
-// map(dune, function(a){ return a});
-//
-// map([1, 2, 3, -9], function(a){ return 2 * a });
-//
-// map([1, 2, 3, -9], function(a){return a * a});
-//
-// sourceArray = [1,2,3]
-// reduce(sourceArray, function(e, memo){return e + memo})
+function map(array, callBack) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    newArray.push(callBack(element));
+  }
+  return newArray;
+}
+function reduce(array, callBack, startingPoint) {
+  let val;
+  if (startingPoint !== undefined) {
+    val = startingPoint;
+    for (const element of array) {
+      val = callBack(val, element);
+    }
+  } else {
+    val = array[0];
+    array = array.slice(1);
+    for (const element of array) {
+      val = callBack(val, element);
+    }
+  }
+  return val;
+}
