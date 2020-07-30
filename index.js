24cf208d393cc752548f5b4f39f9bfc22c0c8cf4
randa yahya <randaalrayan96@gmial.com>
2020-07-30 00:25:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your functions here
+[1, 2, 3, -9].map(function(a) {
+   return -1 * a
+ });
+// function mapToNegativize(src) {
+//   let r = []
+//   for (let i = 0; i < src.length; i++ ) {
+//     r.push(-1 * src[i]) // Unique work
+//   }
+//   return r
+// }
+
+
+// dune = ["paul", "gurney", "vladimir", "jessica", "chani"]
+// map(dune, function(a){ return a});
+//
+// map([1, 2, 3, -9], function(a){ return 2 * a });
+//
+// map([1, 2, 3, -9], function(a){return a * a});
+//
+// sourceArray = [1,2,3]
+// reduce(sourceArray, function(e, memo){return e + memo})
